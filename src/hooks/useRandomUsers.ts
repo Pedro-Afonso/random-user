398ceb5623fb4ad002ab61f4f0cfa3c343cc87c9
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-25 14:44:17 -0300

    feat: Add useRandomUsers hook

---
+++ src/hooks/useRandomUsers.ts
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
+
+import { Data } from '@/pages/api/random-user'
+import { apiClient } from '@/services'
+
+export const useRandomUsers = () => {
+  const router = useRouter()
+
+  const randomUsersData = useQuery<Data>(
+    ['random-user', router.query],
+    () =>
+      apiClient
+        .get('random-user', { params: router.query })
+        .then(res => res.data),
+    {
+      staleTime: 120000
+    }
+  )
+
+  const page = router.query.page
+  const limit = router.query.limit
+
+  return { randomUsersData, page, limit }
+}
