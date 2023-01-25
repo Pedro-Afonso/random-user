fb319e5b3aa9bc48ee1755f2333f5c567f459e45
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-25 14:45:40 -0300

    feat: Add Checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import styles from './Checkbox.module.css'
+
+interface ICheckboxProps {
+  value: string
+  defaultChecked: boolean
+  handleCheckbox: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const Checkbox: React.FC<ICheckboxProps> = ({
+  value,
+  defaultChecked,
+  handleCheckbox
+}) => {
+  return (
+    <label className={styles['form-control']}>
+      <input
+        type="checkbox"
+        name={value}
+        value={value}
+        onChange={handleCheckbox}
+        checked={defaultChecked}
+      />
+      {value}
+    </label>
+  )
+}
