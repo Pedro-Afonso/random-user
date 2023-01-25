c9b2c4af5bb1e478b0606f718aef5361b79b8389
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-25 14:43:33 -0300

    feat: Add Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,30 @@
+import { IRandomUser } from '@/interface'
+import Image from 'next/image'
+import styles from './Card.module.css'
+
+interface ICardProps {
+  randomUser: IRandomUser
+}
+export const Card: React.FC<ICardProps> = ({ randomUser }) => {
+  const {
+    picture: { medium },
+    name: { first, last },
+    location: { street, state, city, postcode },
+    dob
+  } = randomUser
+
+  return (
+    <article className={styles.card}>
+      <section>
+        <Image src={medium} alt={first} width={80} height={80} priority />
+        <h3>{first + last}</h3>
+        <span>Idade: {dob.age}</span>
+        <address>
+          <p>{street.name + street.number}</p>
+          <p>{city}</p>
+          <p>{state + '- CEP:' + postcode}</p>
+        </address>
+      </section>
+    </article>
+  )
+}
