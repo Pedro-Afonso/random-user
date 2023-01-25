092a06fe4c2d8d3ec59a71d9afa1ff16bcca0051
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-25 14:47:36 -0300

    feat: Add Select component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,32 @@
+import { useRouterParams } from '@/hooks'
+import { useState } from 'react'
+import styles from './Select.module.css'
+
+interface ISelectProps {
+  options?: { name: string; value: string }[]
+  onChange: (value: string) => void
+}
+
+export const Select: React.FC<ISelectProps> = ({ options, onChange }) => {
+  const { setSortParam } = useRouterParams()
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortParam('sort', e.target.value)
+  }
+
+  return (
+    <>
+      <div className={styles['select-wrapper']}>
+        <select onChange={handleSelect}>
+          <option value=""></option>
+          {options?.map(opt => (
+            <option key={opt.value} value={opt.value}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+        <span className={styles['focus']}></span>
+      </div>
+    </>
+  )
+}
