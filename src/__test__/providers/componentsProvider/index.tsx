762326d791821457486fe5edaa7721a09ea5d7ea
Pedro-Afonso <pedro.766@hotmail.com>
2023-02-14 11:22:02 -0300

    feat: Add providers

---
+++ src/__test__/providers/componentsProvider/index.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react'
+
+import { PropsWithChildren } from 'react'
+import { AppFilterProvider } from '@/contexts'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '@/services'
+
+export function renderWithProviders(ui: React.ReactElement) {
+  function Wrapper({ children }: PropsWithChildren ): JSX.Element {
+    return (
+      <AppFilterProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </AppFilterProvider>
+    )
+  }
+
+  return { ...render(ui, { wrapper: Wrapper }) }
+}
