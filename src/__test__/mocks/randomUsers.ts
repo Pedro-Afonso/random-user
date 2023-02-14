d74202b52e41f9b8d187d09671b4228eead275ae
Pedro-Afonso <pedro.766@hotmail.com>
2023-02-14 11:21:50 -0300

    feat: Add mocks

---
+++ src/__test__/mocks/randomUsers.ts
@@ -0,0 +1,358 @@
+import { IRandomUser } from '@/interfaces'
+
+export const randomUsers: IRandomUser[] = [
+  {
+    gender: 'female',
+    name: { title: 'Mrs', first: 'Adelaide', last: 'Peixoto' },
+    location: {
+      street: { number: 4797, name: 'Rua Boa Vista ' },
+      city: 'Foz do Iguaçu',
+      state: 'Roraima',
+      country: 'Brazil',
+      postcode: 40949,
+      coordinates: { latitude: '-35.2116', longitude: '-117.3869' },
+      timezone: { offset: '-3:30', description: 'Newfoundland' }
+    },
+    email: 'adelaide.peixoto@example.com',
+    login: {
+      uuid: 'd524bc67-6602-467a-a748-6a177bb18c59',
+      username: 'silvertiger840',
+      password: 'counter',
+      salt: 'QkDnPpLc',
+      md5: '340fdd0faaaae7cffd42049577da62f1',
+      sha1: 'db7f89bb5652db43da670fada8403e2256186fe8',
+      sha256: '0251143198e676676dffb73a6d609c91d85cc2d7c04abefefe2c6c4559239d4f'
+    },
+    dob: { date: '1946-06-22T06:09:00.970Z', age: 76 },
+    registered: { date: '2006-02-16T20:54:20.003Z', age: 16 },
+    phone: '(63) 3053-1982',
+    cell: '(23) 9114-0890',
+    id: { name: 'CPF', value: '394.593.963-04' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/81.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/81.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/81.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'male',
+    name: { title: 'Mr', first: 'Laudelino', last: 'Gomes' },
+    location: {
+      street: { number: 4874, name: 'Rua Dezenove de Outubro' },
+      city: 'Macaé',
+      state: 'Amazonas',
+      country: 'Brazil',
+      postcode: 38560,
+      coordinates: { latitude: '-35.2185', longitude: '100.0870' },
+      timezone: { offset: '+6:00', description: 'Almaty, Dhaka, Colombo' }
+    },
+    email: 'laudelino.gomes@example.com',
+    login: {
+      uuid: '0bfbe883-d553-4049-aff0-2f0d8d038399',
+      username: 'ticklishleopard414',
+      password: 'kane',
+      salt: 'xV7L8btE',
+      md5: '1643ef9df9ec989651f5af9a47a57192',
+      sha1: 'a4d40478e42e2c5ac52de402af6deceefc540c6b',
+      sha256: 'a250cd89f848d194ded279f2ff730212da9848aef2d8847c953b94dd231abb2c'
+    },
+    dob: { date: '1947-09-17T13:53:44.358Z', age: 75 },
+    registered: { date: '2021-09-19T01:12:18.201Z', age: 1 },
+    phone: '(59) 2127-2115',
+    cell: '(42) 3809-9638',
+    id: { name: 'CPF', value: '100.993.468-64' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/95.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/95.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/95.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'female',
+    name: { title: 'Miss', first: 'Dafne', last: 'da Luz' },
+    location: {
+      street: { number: 7397, name: 'Rua São Paulo ' },
+      city: 'Nossa Senhora do Socorro',
+      state: 'Minas Gerais',
+      country: 'Brazil',
+      postcode: 78797,
+      coordinates: { latitude: '68.6601', longitude: '58.1688' },
+      timezone: { offset: '-10:00', description: 'Hawaii' }
+    },
+    email: 'dafne.daluz@example.com',
+    login: {
+      uuid: '925fde48-e0b4-42bb-93be-5c5658ca9f89',
+      username: 'lazyleopard492',
+      password: 'sisters',
+      salt: 'nQEnLgtN',
+      md5: '72d68231ea9494e72670322074eab822',
+      sha1: '328eae305c6e2e6a9a894087d86b724d126d4adb',
+      sha256: '51e72481aac73a4198b6992a47b897231b0b2c8b26b9cbdac0c79dff64db7d7b'
+    },
+    dob: { date: '1948-11-23T17:00:34.781Z', age: 74 },
+    registered: { date: '2016-07-11T12:10:59.500Z', age: 6 },
+    phone: '(95) 6775-4399',
+    cell: '(50) 8320-9473',
+    id: { name: 'CPF', value: '250.402.636-93' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/33.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/33.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/33.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'female',
+    name: { title: 'Ms', first: 'Alísia', last: 'Lima' },
+    location: {
+      street: { number: 5726, name: 'Rua Vinte de Setembro' },
+      city: 'Campo Largo',
+      state: 'Santa Catarina',
+      country: 'Brazil',
+      postcode: 67384,
+      coordinates: { latitude: '11.3430', longitude: '-65.8951' },
+      timezone: {
+        offset: '+10:00',
+        description: 'Eastern Australia, Guam, Vladivostok'
+      }
+    },
+    email: 'alisia.lima@example.com',
+    login: {
+      uuid: 'a28a607c-3512-4765-9998-2de719a7a2f3',
+      username: 'bluefrog661',
+      password: 'francine',
+      salt: 'aM8H4NU4',
+      md5: '761665bb941c63356bf32638c5c4d2a7',
+      sha1: 'c8d5045bacdd9df794117a55baa4be0ada33b9ae',
+      sha256: '6d7bb094eb6477b8da2ba911116c734835f61ef635a02646eaa5b51da877a404'
+    },
+    dob: { date: '1948-05-17T03:32:41.221Z', age: 74 },
+    registered: { date: '2007-09-17T20:38:56.559Z', age: 15 },
+    phone: '(98) 8847-4229',
+    cell: '(26) 0203-1759',
+    id: { name: 'CPF', value: '885.702.839-34' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/64.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/64.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/64.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'male',
+    name: { title: 'Mr', first: 'Jacinto', last: 'Porto' },
+    location: {
+      street: { number: 6306, name: 'Rua Santa Maria ' },
+      city: 'Jequié',
+      state: 'Sergipe',
+      country: 'Brazil',
+      postcode: 37394,
+      coordinates: { latitude: '-63.3617', longitude: '-45.7408' },
+      timezone: {
+        offset: '-3:00',
+        description: 'Brazil, Buenos Aires, Georgetown'
+      }
+    },
+    email: 'jacinto.porto@example.com',
+    login: {
+      uuid: '5ca1f7e0-7b1a-4968-b00b-55b428c5aa7e',
+      username: 'happygorilla126',
+      password: 'iguana',
+      salt: 'kYtktSVx',
+      md5: '695acca4f09d34bb33f83fc486f7ca2b',
+      sha1: 'ffc95cec03b63408317765c17c742d61fa585a82',
+      sha256: '94afa208901dd7f15d71dda2263bce0071232ebcb8afaca371af76ce00e34bac'
+    },
+    dob: { date: '1951-12-21T15:41:29.881Z', age: 71 },
+    registered: { date: '2010-03-29T11:45:11.472Z', age: 12 },
+    phone: '(69) 4253-6044',
+    cell: '(77) 1573-6889',
+    id: { name: 'CPF', value: '541.362.603-32' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/48.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/48.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/48.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'female',
+    name: { title: 'Ms', first: 'Mafalda', last: 'da Cruz' },
+    location: {
+      street: { number: 4016, name: 'Rua Santo Antônio ' },
+      city: 'Recife',
+      state: 'Ceará',
+      country: 'Brazil',
+      postcode: 76906,
+      coordinates: { latitude: '87.4229', longitude: '-108.3441' },
+      timezone: { offset: '-7:00', description: 'Mountain Time (US & Canada)' }
+    },
+    email: 'mafalda.dacruz@example.com',
+    login: {
+      uuid: 'fa26f646-145a-4452-b5b9-42b3d80fb662',
+      username: 'whiteleopard515',
+      password: 'bigbird',
+      salt: 'x52hRKxf',
+      md5: '897abc95f378f773fc6b1bbb3e8b9453',
+      sha1: 'b2c8c7cd840f5923f1101c952b21208962660a3e',
+      sha256: '8d58fec0a3995feaa1669885cd3793f839e026f84a44bd67d7ad644f1efbb1a9'
+    },
+    dob: { date: '1951-08-31T23:37:49.715Z', age: 71 },
+    registered: { date: '2007-05-19T08:23:43.972Z', age: 15 },
+    phone: '(36) 4636-8598',
+    cell: '(94) 9165-2747',
+    id: { name: 'CPF', value: '769.945.002-58' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/70.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/70.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/70.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'female',
+    name: { title: 'Mrs', first: 'Aimée', last: 'das Neves' },
+    location: {
+      street: { number: 6975, name: 'Rua Rui Barbosa ' },
+      city: 'Itatiba',
+      state: 'Distrito Federal',
+      country: 'Brazil',
+      postcode: 80904,
+      coordinates: { latitude: '1.7719', longitude: '138.0062' },
+      timezone: { offset: '+7:00', description: 'Bangkok, Hanoi, Jakarta' }
+    },
+    email: 'aimee.dasneves@example.com',
+    login: {
+      uuid: '056cb963-c56d-46d4-a3ea-c52959747362',
+      username: 'purplepeacock407',
+      password: 'tongue',
+      salt: 'vzZz3rzn',
+      md5: '9de28402fbbccb152c0275b0150f0c92',
+      sha1: '092a5551cc0c4313009707f501c24b67d6f7ab8d',
+      sha256: 'c0814704d4022f9c7a504fd1a01f6b1b17adf85ffb01c93333c50d6f4a861e9b'
+    },
+    dob: { date: '1952-03-08T02:31:01.907Z', age: 70 },
+    registered: { date: '2013-08-05T17:02:25.371Z', age: 9 },
+    phone: '(64) 2414-1133',
+    cell: '(76) 1949-0470',
+    id: { name: 'CPF', value: '653.540.073-37' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/31.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/31.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/31.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'female',
+    name: { title: 'Mrs', first: 'Lias', last: 'da Rocha' },
+    location: {
+      street: { number: 5282, name: 'Rua Santa Maria ' },
+      city: 'Itapevi',
+      state: 'Acre',
+      country: 'Brazil',
+      postcode: 53398,
+      coordinates: { latitude: '22.9157', longitude: '-178.8100' },
+      timezone: { offset: '-8:00', description: 'Pacific Time (US & Canada)' }
+    },
+    email: 'lias.darocha@example.com',
+    login: {
+      uuid: 'c3464191-29af-4940-b44a-f0b544cb2583',
+      username: 'purplemeercat580',
+      password: 'stayout',
+      salt: 'YlvfiDAw',
+      md5: '989fab95957530c8d0d04de2be25f140',
+      sha1: 'd55a84e529afb4874c5197445036c86880436187',
+      sha256: '5211720a0d2374d1ae992eee0839594553f6eda4603185cb827af789f2cb2fa3'
+    },
+    dob: { date: '1953-04-16T07:23:38.882Z', age: 69 },
+    registered: { date: '2018-11-06T18:11:13.474Z', age: 4 },
+    phone: '(40) 5646-1921',
+    cell: '(19) 2958-2566',
+    id: { name: 'CPF', value: '215.248.518-09' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/87.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/87.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/87.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'male',
+    name: { title: 'Mr', first: 'Osvaldo', last: 'Pinto' },
+    location: {
+      street: { number: 1925, name: 'Rua da Saudade' },
+      city: 'Blumenau',
+      state: 'Bahia',
+      country: 'Brazil',
+      postcode: 95922,
+      coordinates: { latitude: '57.5149', longitude: '136.0349' },
+      timezone: {
+        offset: '-3:00',
+        description: 'Brazil, Buenos Aires, Georgetown'
+      }
+    },
+    email: 'osvaldo.pinto@example.com',
+    login: {
+      uuid: '9ba65f1c-fa21-4824-81c3-21382107fe57',
+      username: 'ticklishsnake922',
+      password: '888888',
+      salt: '0XaPdWPO',
+      md5: '1f98bcca7250f3174947875480eaac40',
+      sha1: '0ad7c784e9c943b63931bec8d1f90505ed580ac7',
+      sha256: '7f7035a8b526386472f8053f3bf54e73bb0d9ec7cfb353b2a9367850851977cd'
+    },
+    dob: { date: '1955-04-13T23:43:48.438Z', age: 67 },
+    registered: { date: '2018-09-26T07:37:43.826Z', age: 4 },
+    phone: '(28) 1353-4189',
+    cell: '(22) 7672-3831',
+    id: { name: 'CPF', value: '507.848.290-22' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/85.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/85.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/85.jpg'
+    },
+    nat: 'BR'
+  },
+  {
+    gender: 'male',
+    name: { title: 'Mr', first: 'Nídio', last: 'Pires' },
+    location: {
+      street: { number: 2599, name: 'Avenida Brasil ' },
+      city: 'Novo Hamburgo',
+      state: 'Maranhão',
+      country: 'Brazil',
+      postcode: 22213,
+      coordinates: { latitude: '8.7083', longitude: '-97.2667' },
+      timezone: {
+        offset: '+10:00',
+        description: 'Eastern Australia, Guam, Vladivostok'
+      }
+    },
+    email: 'nidio.pires@example.com',
+    login: {
+      uuid: '610f8eeb-5675-4dc0-a037-28e2a716c0a8',
+      username: 'organicswan508',
+      password: 'django',
+      salt: 'RjxOankI',
+      md5: '98329d92d386f9b548eef3b5d1496fb3',
+      sha1: 'edabdfb309b57f070b99779f02785108b7d903bc',
+      sha256: '4e7d642f562f7b253d08e493d787d44fed64815aaabb0209dcec38695e634d8b'
+    },
+    dob: { date: '1957-08-26T05:00:59.827Z', age: 65 },
+    registered: { date: '2003-02-08T23:13:14.890Z', age: 20 },
+    phone: '(78) 6162-7331',
+    cell: '(69) 1422-1006',
+    id: { name: 'CPF', value: '144.417.305-09' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/94.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/94.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/94.jpg'
+    },
+    nat: 'BR'
+  }
+]
+
+export const randomUser = randomUsers[0]
