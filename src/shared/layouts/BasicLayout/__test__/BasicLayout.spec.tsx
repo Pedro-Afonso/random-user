b5978a2c56e98814294e6b85e0b75f56d8d8a739
Pedro-Afonso <pedro.766@hotmail.com>
2023-02-16 18:05:58 -0300

    test: Add layout test

---
+++ src/shared/layouts/BasicLayout/__test__/BasicLayout.spec.tsx
@@ -0,0 +1,33 @@
+import { fireEvent, screen } from '@testing-library/react'
+import mockRouter from 'next-router-mock'
+
+import { renderWithProviders } from '@/providers'
+
+import { BasicLayout } from '..'
+
+jest.mock('next/router', () => require('next-router-mock'))
+
+describe('BasicLayout', () => {
+  const props = {
+    title: 'title-test',
+    subtitle: 'subtitle-test',
+    listingTool: <p>listingTool-test</p>,
+    filterList: <p>filterList-test</p>,
+    children: <p>children-test</p>
+  }
+  it('should render BasicLayout', () => {
+    renderWithProviders(<BasicLayout {...props} />)
+
+    const title = screen.getByText('title-test')
+    const subtitle = screen.getByText('subtitle-test')
+    const listingTool = screen.getByText('listingTool-test')
+    const filterList = screen.getByText('filterList-test')
+    const children = screen.getByText('children-test')
+
+    expect(title).toBeInTheDocument()
+    expect(subtitle).toBeInTheDocument()
+    expect(listingTool).toBeInTheDocument()
+    expect(filterList).toBeInTheDocument()
+    expect(children).toBeInTheDocument()
+  })
+})
